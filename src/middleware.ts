bdb7cf3737bb1c9a1043dad34e794b242b9310b9
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-07-04 23:15:59 +0100

    Hopterlink

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: ["/business/:path*", "/account"],
-};
+// export const config = {
+//   matcher: ["/business/:path*", "/account"],
+// };
