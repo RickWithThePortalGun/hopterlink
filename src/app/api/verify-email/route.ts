f0be56a5ae7e3ed1068bc18215a1d3e7e8310e2e
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-07-22 17:32:08 +0100

    Verification page completed

---
+++ src/app/api/verify-email/route.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const uri = `${process.env.NEXTAUTH_BACKEND_URL}auth/registration/verify-email/`;
+    const result = await axios.post(uri, {
+      key: body.key,
+    });
+    console.log(result);
+    return NextResponse.json(result.data, { status: 200 });
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json(error, { status: 400 });
+  }
+}
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -38,7 +38,7 @@ const SignUpForm = () => {
         .regex(/\d/, "Password must contain at least one number")
         .regex(
           /[^a-zA-Z0-9]/,
-          "Password must contain at least one special character"
+          "Password must contain at least one special character",
         ),
       password2: z
         .string()
