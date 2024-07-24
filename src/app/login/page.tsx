c7a3bcc013d188da8f00726f7c27609028c86b4a
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-07-24 09:41:27 +0100

    fixed login loop

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-
 export default function AuthenticationPage() {
   const { status } = useSession();
   const router = useRouter();
