b924c212babac6b2b85ed296b6e319d3da473003
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-07-24 14:36:16 +0100

    Integrated business details

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
-    callbacks: {
-      authorized: ({ req }) => {
-        // verify token and return a boolean
-        const sessionToken = req.cookies.get("next-auth.session-token");
-        if (sessionToken) return true;
-        else return false;
-      },
+  callbacks: {
+    authorized: ({ req }) => {
+      // verify token and return a boolean
+      const sessionToken = req.cookies.get("next-auth.session-token");
+      if (sessionToken) return true;
+      else return false;
     },
-  });
+  },
+});
 export const config = {
   matcher: ["/business/:path*"],
 };
