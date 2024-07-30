abeeff225c778ba6fd4b8d66f81749665a4b55bf
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-07-30 01:37:10 +0100

    review fix and signup mod

--- src/app/api/reviews/[id]/route.ts
+++ src/app/api/reviews/[id]/route.ts
@@ -1,35 +1,31 @@
+import axios from "axios";
+import { useSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 import request from "@/utils/http-request";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import axios from "axios";
+
+
 
 export async function POST(
   req: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
   const body = await req.json();
+  console.log(body)
   const { id } = params;
   const uri = `${process.env.NEXT_PUBLIC_BACKEND_URL}api/businesses/${id}/reviews`;
-  const reviews = await axios.post(
-    uri,
-    { rating: body.stars, comment: body.content },
-    {
-      headers: {
-        Authorization: `Token ${session?.access_token}`,
-      },
-    },
-  );
-  console.log("Reviews endpoint: ", reviews);
-
-  if (reviews) {
+  console.log("Review endpoint hit");
+  try {
+    const reviews = await request.post(uri, {
+      rating: body.stars,
+      comment: body.content,
+    });
     console.log("Reviews endpoint: ", reviews);
     return NextResponse.json(reviews.data);
-  } else {
+  } catch (error) {
+    console.log("Error found: ", error);
     return NextResponse.json(
       { message: "Your review was not submitted." },
-      { status: 404 },
+      { status: 404 }
     );
   }
 }
