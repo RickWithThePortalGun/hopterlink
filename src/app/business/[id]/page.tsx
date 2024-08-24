fa125c09ebeb9f7e22225c30acdcf555b787d78d
Oyeniyi Victor <87849462+RickWithThePortalGun@users.noreply.github.com>
2024-08-24 23:34:34 +0100

    Button size adjustment

--- src/app/business/[id]/page.tsx
+++ src/app/business/[id]/page.tsx
@@ -310,7 +310,7 @@ const Business = ({ params }: Props) => {
                 <Share size={16} /> Share
               </Button>
             </div>
-            <div className="flex flex-row max-lg:flex-col items-center gap-4">
+            <div className="flex flex-row max-lg:flex-col items-center gap-2">
               {isFavorite ? (
                 <Button
                   className="flex min-w-60 gap-2 items-center max-md:mt-2"
